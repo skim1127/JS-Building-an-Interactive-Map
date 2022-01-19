096d602c91b655de48a3b7c0037e56451c47811d
Jin mo Kim <skim112790@yahoo.com>
2022-01-18 19:54:44 -0500

    added a map with custom pins

--- public/app.js
+++ public/app.js
@@ -1,19 +1,47 @@
 // create map
+var myMap = L.map('map', {
+    center: [48.868672, 2.342130],
+    zoom: 12,
+});
 
+// Add OpenStreetMap tiles:
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: '15',
+}).addTo(myMap)
 
-// add openstreetmap tiles
+// Create and add a geolocation marker:
+const marker = L.marker([48.87007, 2.346453])
+marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
 
-
-// create and main add geolocation marker
-
-
-// draw the 2nd arrondissement
+// draw a polygon based on coordinates
+var polygon = L.polygon([
+    [48.863368120198004, 2.3509079846928516],
+    [48.86933262048345, 2.3542531602919805],
+    [48.87199261164275, 2.3400569901592183],
+    [48.86993336274516, 2.3280142476578813],
+    [48.86834104280146, 2.330308418109664]
+]).addTo(myMap);
 
 
 // create red pin marker
+    //create variable for icon
+var myIcon = L.icon({
+    iconUrl: './assets/red-pin.png',
+    iconSize: [38,38],
+    iconAnchor: [19,38],
+})
 
+// Metro station markers:
+    //create markers
+const rS = L.marker([48.866200610611926, 2.352236247419453],{icon: myIcon}).bindPopup('Réaumur-Sébastopol')
+const sSD = L.marker([48.869531786321566, 2.3528590208055196],{icon: myIcon}).bindPopup('Strasbourg-Saint-Denis')
+const sentier = L.marker([48.8673721067762, 2.347107922912739],{icon: myIcon}).bindPopup('Sentier')
+const bourse = L.marker([48.86868503971672, 2.3412285142058167],{icon: myIcon}).bindPopup('Bourse')
+const qS = L.marker([48.869560129483226, 2.3358638645569543],{icon: myIcon}).bindPopup('Quatre Septembre')
+const gB = L.marker([48.871282159004856, 2.3434818588892714],{icon: myIcon}).bindPopup('Grands Boulevards')
 
-// metro station markers
-
+    //Add markers to a layer group by using an array
+const stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(myMap)
 
 
